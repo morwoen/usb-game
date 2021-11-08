599b37ffc2bca1c027fae1045c166a89c6911e93
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2021-11-08 00:17:28 +0000

    Replace Tween Lib

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction
+{
+  public static void Interact(float time) {
+
+  }
+
+  public static void Interrupt() {
+
+  }
+}
