9f1614b689a339186e7d3616088fee257ae469a9
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2021-11-25 00:25:32 +0000

    Progress and credits section of the menu

---
+++ Assets/Scripts/Menu/MenuMissionPanelRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuMissionPanelRenderer : MonoBehaviour
+{
+  [SerializeField]
+  private TextMeshProUGUI spritePanel;
+  [SerializeField]
+  private TextMeshProUGUI body;
+  [SerializeField]
+  private Color completedColor;
+
+  public void Render(Mission mission) {
+    bool completed = DataManager.IsCompleted(mission);
+    if (completed) {
+      body.text = mission.description;
+      spritePanel.text = "O";
+      spritePanel.color = completedColor;
+    } else {
+      body.text = "Explore the game to complete this mission";
+    }
+  }
+}
---
+++ Assets/Scripts/Menu/ProgressMenuPanel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressMenuPanel : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject prefab;
+  [SerializeField]
+  private MissionDatabase database;
+
+  private void Awake() {
+    foreach (Mission mission in database.missions) {
+      MenuMissionPanelRenderer renderer = Instantiate(prefab, transform).GetComponent<MenuMissionPanelRenderer>();
+      renderer.Render(mission);
+    }
+  }
+}
